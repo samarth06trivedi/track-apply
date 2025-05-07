d41d71030d733bea3e87c865363f3d65090e9a27
samarth06trivedi <samarth2004trivedi@gmail.com>
2025-05-07 11:50:17 +0530

    .

--- src/app/api/job-applications/route.ts
+++ src/app/api/job-applications/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 import { users, jobApplications } from "@/lib/db";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 
 // POST request for creating a new job application
 export async function POST(req: Request) {
